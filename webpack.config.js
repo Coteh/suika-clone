95a1874059b6fd449fac32fa655637097ade10a8
Coteh <3276350+Coteh@users.noreply.github.com>
2023-10-12 17:53:07 -0400

    Update webpack to v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
 const path = require('path');
-const pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
-const phaser = path.join(pathToPhaser, 'dist/phaser.js');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (env) => {
     const isDebug = env && (env.NODE_ENV === 'dev' || env.NODE_ENV === 'test');
 
     return {
         entry: './src/game.ts',
+        mode: env.NODE_ENV === 'dev' ? 'development' : 'production',
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: 'bundle.js',
+            clean: true,
         },
         module: {
             rules: [
@@ -26,12 +25,14 @@ module.exports = (env) => {
                             },
                         },
                     ],
-                    exclude: '/node_modules/',
+                    exclude: /node_modules/,
                 },
             ],
         },
         devServer: {
-            contentBase: path.resolve(__dirname, './dist'),
+            static: {
+                directory: path.resolve(__dirname, './dist'),
+            },
             host: '0.0.0.0',
             port: 8080,
             open: true,
@@ -39,24 +40,26 @@ module.exports = (env) => {
         resolve: {
             extensions: ['.ts', '.js'],
             alias: {
-                phaser: phaser,
+                phaser: path.resolve(
+                    __dirname,
+                    'node_modules/phaser/dist/phaser.js'
+                ),
             },
         },
         plugins: [
-            new CleanWebpackPlugin({
-                verbose: true,
-            }),
             new webpack.DefinePlugin({
                 'process.env.IS_DEBUG': JSON.stringify(isDebug),
             }),
-            new CopyWebpackPlugin([
-                { from: 'assets/img/**', to: '.' },
-                { from: 'index.html', to: '.' },
-                { from: 'index.css', to: '.' },
-                { from: 'favicon.ico', to: '.' },
-                { from: 'icon*.png', to: '.' },
-                { from: 'manifest.json', to: '.' },
-            ]),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'assets/img/**', to: '.' },
+                    { from: 'index.html', to: '.' },
+                    { from: 'index.css', to: '.' },
+                    { from: 'favicon.ico', to: '.' },
+                    { from: 'icon*.png', to: '.' },
+                    { from: 'manifest.json', to: '.' },
+                ],
+            }),
         ],
     };
 };
